a324717520f7a928db0a07e6e28d0fd97ebeb96d
Santosh Yadav <ymanoutube@gmail.com>
2023-08-05 16:33:48 +0545

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 import netlify from "@astrojs/netlify/functions";
@@ -8,6 +8,4 @@ export default defineConfig({
   integrations: [react()],
   output: "server",
   adapter: netlify(),
-  site: 'https://S29antosh.github.io',
-  base: '/Playing with-Astro'
 });
